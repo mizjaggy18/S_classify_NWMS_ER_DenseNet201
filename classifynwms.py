4a092683ac92654c3b308b5765b732b6962107fe
mizjaggy18 <77017051+mizjaggy18@users.noreply.github.com>
2021-11-04 00:16:57 +0800

    Update classifynwms.py

--- classifynwms.py
+++ classifynwms.py
@@ -49,7 +49,7 @@ import math
 import argparse
 import json
 import logging
-import pathlib
+# import pathlib
 
 
 
@@ -100,17 +100,17 @@ def main(argv):
 
 #         model_directory = os.path.join(base_path,'models/ModelDenseNet201')
 #         model_directory = working_path
-#         model_directory = '/models/ModelDenseNet201'
+        model_directory = '/models/ModelDenseNet201'
 
         # model_name = 'densenet201weights.best.h5'
 #         model_dir = pathlib.Path("weights_float16/")
 #         
 #        print('current working dir:',pathlib.Path.cwd())
-        model_dir = pathlib.Path.cwd()
+#         model_dir = pathlib.Path.cwd()
         model_name = 'weights.best.h5'
 #         model_file = pathlib.Path("model_quant_f16.tflite")
 #         model_file = model_dir/"model_quant_f16.tflite"  
-        model_file = model_dir/model_name
+#         model_file = model_dir/model_name
 #         model_name = 'model_quant_f16.tflite'
 
         
