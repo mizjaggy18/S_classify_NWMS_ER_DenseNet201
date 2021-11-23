04890c3d1d60981665b1d9b921d8767c7d3c7949
mizjaggy18 <77017051+mizjaggy18@users.noreply.github.com>
2021-11-23 10:48:58 +0800

    Update classifynwms.py

--- classifynwms.py
+++ classifynwms.py
@@ -45,6 +45,7 @@ import matplotlib.pyplot as plt
 import time
 import cv2
 import math
+import h5py
 
 import argparse
 import json
@@ -57,29 +58,29 @@ __author__ = "WSH Munirah W Ahmad <wshmunirah@gmail.com>"
 __version__ = "1.0.0"
 # Date created: 03 June 2021 (modified on 15 Oct 2021 for prune)
 
-# def densemodel():
-#     data_augmentation = tf.keras.Sequential([
-#         tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-#         tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-#         tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
-#     ])
-#     tensor = tf.keras.Input((224, 224, 3))
-#     x = tf.cast(tensor, tf.float32)
-#     x = tf.keras.applications.densenet.preprocess_input(
-#         x, data_format=None)
-#     x = data_augmentation(x)
-#     x = pretrained_model(x, training=False)
-#     x = tf.keras.layers.Flatten()(x)
-#     x = tf.keras.layers.Dense(256)(x)
-#     x = tf.nn.relu(x)
-#     x = tf.keras.layers.Dropout(0.4)(x)
-#     x = tf.keras.layers.Dense(4)(x)
-#     x = tf.nn.softmax(x)
-#     model = tf.keras.Model(tensor, x)
-#     model.compile(optimizer=tf.keras.optimizers.Adam(),
-#                   loss=tf.keras.losses.CategoricalCrossentropy(),
-#                   metrics=['accuracy'])
-#     return model
+def densemodel():
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+        tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+        tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
+    ])
+    tensor = tf.keras.Input((224, 224, 3))
+    x = tf.cast(tensor, tf.float32)
+    x = tf.keras.applications.densenet.preprocess_input(
+        x, data_format=None)
+    x = data_augmentation(x)
+    x = pretrained_model(x, training=False)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(256)(x)
+    x = tf.nn.relu(x)
+    x = tf.keras.layers.Dropout(0.4)(x)
+    x = tf.keras.layers.Dense(4)(x)
+    x = tf.nn.softmax(x)
+    model = tf.keras.Model(tensor, x)
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=tf.keras.losses.CategoricalCrossentropy(),
+                  metrics=['accuracy'])
+    return model
 
 
 def main(argv):
@@ -120,9 +121,9 @@ def main(argv):
         print('Loading model.....')
 #         print(model_file)
 
-#         model = densemodel()
-#         model.load_weights(current_dir + model_name)
-        model = tf.keras.models.load_model(current_dir + model_name, compile = False)
+        model = densemodel()
+        model.load_weights(current_dir + model_name)
+#         model = tf.keras.models.load_model(current_dir + model_name, compile = False)
         
         # model.load_weights(model_directory + model_name)
 #         model.load_weights(working_path +'/'+ model_name)
